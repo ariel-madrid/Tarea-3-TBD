5ce635a709bf6bd9dacdb0bf01845af63f3ffbb9
Ariel Argomedo <ariel.argomedo@usach.cl>
2022-05-12 21:23:40 -0400

    Modelo Region

---
+++ ejemplo-spring-gis/src/main/java/cl/tbd/ejemplo1/models/Region.java
@@ -0,0 +1,20 @@
+package cl.tbd.ejemplo1.models;
+
+import org.postgis.PGgeometry;
+import org.postgis.Point;
+
+public class Region {
+    private int id;
+    private String nom_reg;
+    private String nom_prov;
+    private String cod_com;
+    private String nom_com;
+    private float cod_regi;
+    private float superficie;
+    private int poblac02;
+    private int pobl2010;
+    private float shape_leng;
+    private float shape_area;
+    private double longitude;
+    private double latitude;
+}
