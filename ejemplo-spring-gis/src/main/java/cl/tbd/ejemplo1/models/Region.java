1162ffb0c60dbb43790227cec9bd97a58c7c45db
dinero450 <ramon.parra@usach.cl>
2022-05-12 21:32:59 -0400

    Se termina el modelo con setters y getters

--- ejemplo-spring-gis/src/main/java/cl/tbd/ejemplo1/models/Region.java
+++ ejemplo-spring-gis/src/main/java/cl/tbd/ejemplo1/models/Region.java
@@ -17,4 +17,86 @@ public class Region {
     private float shape_area;
     private double longitude;
     private double latitude;
+
+    
+    public int getId() {
+        return id;
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+    public double getLongitude() {
+        return longitude;
+    }
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+    public float getShape_area() {
+        return shape_area;
+    }
+    public void setShape_area(float shape_area) {
+        this.shape_area = shape_area;
+    }
+    public float getShape_leng() {
+        return shape_leng;
+    }
+    public void setShape_leng(float shape_leng) {
+        this.shape_leng = shape_leng;
+    }
+    public int getPobl2010() {
+        return pobl2010;
+    }
+    public void setPobl2010(int pobl2010) {
+        this.pobl2010 = pobl2010;
+    }
+    public int getPoblac02() {
+        return poblac02;
+    }
+    public void setPoblac02(int poblac02) {
+        this.poblac02 = poblac02;
+    }
+    public float getSuperficie() {
+        return superficie;
+    }
+    public void setSuperficie(float superficie) {
+        this.superficie = superficie;
+    }
+    public float getCod_regi() {
+        return cod_regi;
+    }
+    public void setCod_regi(float cod_regi) {
+        this.cod_regi = cod_regi;
+    }
+    public String getNom_com() {
+        return nom_com;
+    }
+    public void setNom_com(String nom_com) {
+        this.nom_com = nom_com;
+    }
+    public String getCod_com() {
+        return cod_com;
+    }
+    public void setCod_com(String cod_com) {
+        this.cod_com = cod_com;
+    }
+    public String getNom_prov() {
+        return nom_prov;
+    }
+    public void setNom_prov(String nom_prov) {
+        this.nom_prov = nom_prov;
+    }
+    public String getNom_reg() {
+        return nom_reg;
+    }
+    public void setNom_reg(String nom_reg) {
+        this.nom_reg = nom_reg;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
 }
+
+
