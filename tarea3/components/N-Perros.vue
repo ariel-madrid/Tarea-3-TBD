b88fd3ff47c060142dc5e16e7a5b90ba402df602
mguzm4n <marcelo.guzman.g@usach.cl>
2022-05-15 17:51:58 -0400

    maquetar los estilos y funcionalidad de componente N-Peros

--- tarea3/components/N-Perros.vue
+++ tarea3/components/N-Perros.vue
@@ -1,12 +1,30 @@
 <template>
-    <div class="grid grid-cols-1 bg-white shadow-sm p-5 rounded w-80 ml-2 border border-gray-200 h-64 place-content-center">
+    <div class="grid grid-cols-1 bg-white shadow-sm p-5 rounded w-80 ml-2 border border-gray-200 place-content-center">
      
 
-        <div class="mt-10 flex justify-center">
-            Aqui va el mapa
-        </div>
+        <div class="mt-10 flex flex-col justify-center">
+            <p>{{ message }}</p>
 
-        <button @click="emit" class="bg-blue-500 rounded mt-10 w-full text-gray-700 block px-4 py-2 text-sm hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">Volver</button>
+            <label for="nDogs">Seleccione la cantidad N de perros a obtener:</label>
+            <input class="border-solid border-2" 
+                   v-model="n"
+                   id="nDogs" type="number" value="1"/>
+            <p>{{ this.n == null || this.n == ""
+                  ? "Seleccione N"
+                  : `N: ${this.n} perros` 
+               }}
+            </p>
+            <button class="bg-blue-500 rounded mt-10 w-full text-gray-700 py-2" 
+                    @click="getNNearDogs()">
+                    Get!
+            </button>
+        </div>
+        
+
+        <button @click="emit" 
+                class="bg-blue-500 rounded mt-10 w-full text-gray-700 block px-4 py-2 text-sm hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+                Volver
+        </button>
 
     </div>
 </template>
@@ -17,12 +35,28 @@ import axios from "axios";
 export default {
     data() {
         return {
+            dogId: null,
+            n: null,
+            message: "",
             value: false
         };
     },
     methods: {
-        emit()
-        {
+        async getNNearDogs(){
+            this.dogId = 1;
+            this.n = 2;
+            try {
+                let response = await axios.get("http://localhost:8080/dogs/nneardogs", {
+                    params: {dogId: this.dogId, n: this.n}
+                });
+
+                } catch (error) {
+                console.log('error', error); 
+                this.message = 'Ocurrió un error'
+            }
+        },
+        emit(){
+            
             this.$emit('close', true)
         }
     }
