cfab1852bde390826f501f7d496b23cb8cdfcbfb
Ariel Argomedo <ariel.argomedo@usach.cl>
2022-05-16 20:33:09 -0400

    Cambios en estilos

--- tarea3/components/Principal.vue
+++ tarea3/components/Principal.vue
@@ -25,11 +25,10 @@
       <div id="principalmap" class="mt-10">
       </div>
 
-      <div>
+      <div class="m-3">
         {{ point }} 
-        <input type="text" v-model="name" placeholder="nombre" />
-        <button type="button" @click="crearPerro()">Crear</button>
-        <p>{{ selectedPoint }}</p>
+        <input  class="pl-5 px-5 py-2.5 " type="text" v-model="name" placeholder="Nombre perro" />
+        <button class="mt-5 ml-5 mr-5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600" type="button" @click="crearPerro()">Crear</button>
       </div>
     </div>
     
