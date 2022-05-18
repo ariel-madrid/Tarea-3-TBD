4849517850a6f103d85465b7abb90ec3d6f05cf3
Ariel Argomedo <ariel.argomedo@usach.cl>
2022-05-17 20:16:05 -0400

    Version final de la vista obtener perros de una region

--- tarea3/components/Perros-Region.vue
+++ tarea3/components/Perros-Region.vue
@@ -3,7 +3,7 @@
         
         <!-- This example requires Tailwind CSS v2.0+ -->
         <div class="relative inline-block text-left">
-            <div class = "mb-10" id="mapid5"></div>
+            <div class = "mb-3 w-full" id="mapid5"></div>
             <p class="mb-5">La cantidad de perros en la region seleccionada es de: {{contador}}</p>
             <div>
                 <button @click="showCombobox" type="button" class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button">
@@ -134,3 +134,10 @@ export default {
         }
 }
 </script>
+
+<style>
+    #mapid5 {
+        width: 400px;
+        aspect-ratio: 16/9;
+    }
+</style>
